d8cf8b22e7d97193c54e1f65113e3edcf200ca17
Nicholas Dudfield <ndudfield@gmail.com>
2015-06-04 15:59:19 +0700

    Remove custom patches already included in 1.0.3 sjcl: * Remove sjcl-montgomery * Remove ripemd160

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ var sjcl = require('sjcl');
 require('./sjcl-ecc-pointextras.js');
 require('./sjcl-secp256k1.js');
 require('./sjcl-extramath.js');
-// Now in 1.0.3, we could remove this :)
-require('./sjcl-montgomery.js');
 require('./sjcl-validecc.js');
 require('./sjcl-ecdsa-canonical.js');
 require('./sjcl-ecdsa-der.js');
